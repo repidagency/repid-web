8abdf29c12728867ad75a83040259ad3bbf26945
abduraufrasulboyev <abduraufrasulboyev@gmail.com>
2026-08-05 22:00:11 +0500

    done

--- app/(uz)/layout.tsx
+++ app/(uz)/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import LayoutShell, { baseMetadata } from "../LayoutShell";
 
 export const metadata: Metadata = baseMetadata;
@@ -8,5 +9,34 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <LayoutShell lang="uz">{children}</LayoutShell>;
+  return (
+    <LayoutShell lang="uz">
+      {/* 1. Main GTM Script (<head> ga tushadi) */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TG86XMQC');
+          `,
+        }}
+      />
+
+      {/* 2. Noscript fallback (JavaScript o'chirilgan holat uchun) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-TG86XMQC"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
+
+      {children}
+    </LayoutShell>
+  );
 }
